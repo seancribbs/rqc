ac9ccc7a8e2090396b978835ea4f0afb4159eefb
Sean Cribbs <sean@basho.com>
2010-10-31 10:09:14 -0700

    Revert from literal example to using the generator.

--- example.rb
+++ example.rb
@@ -2,7 +2,7 @@ require 'rqc'
 RQC do  
   property "String#to_s equals self", ascii_string do |str|
 #    collect(str.length)
-    assert("Sean".to_s == "Sean")
+    assert(str.to_s == str)
   end
 
   property "Integer squared is an integer greater than 0", int do |i|
